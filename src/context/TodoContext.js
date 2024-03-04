6ea6f31a3d522f8dc8a0d118fe4c85c88a5c8181
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2024-03-04 23:00:46 +0530

    first commit

---
+++ src/context/TodoContext.js
@@ -0,0 +1,21 @@
+import { useContext, createContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+        { 
+            id: 1,
+            todo:"Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo:(todo)=>{},
+    updatedTodo: (todo,id)=>{},
+    deleteTodo: (id)=>{},
+    toggleComplete: (id)=>{}
+});
+
+export default function useTodo() {
+  return useContext(TodoContext);
+}
+
+export const Todoprovider = TodoContext.Provider;
